2ccc54d8515b94e2f9bc1995382ea4c97d4fdae9
Nisheka <tharushinisheka@gmail.com>
2023-08-16 16:51:36 +0530

    4th Commit

--- app.py
+++ app.py
@@ -25,6 +25,50 @@ def home():
 def sign():
     return render_template('sign.html')
 
+@app.route('/Local')
+def Local():
+    return render_template('Local.html')
+    # app.py
+@app.route('/homee')
+def homee():
+    return render_template('homee.html')
+    
+@app.route('/kandy')
+def kandy():
+    return render_template('Kandy.html')
+
+@app.route('/lowc')
+def lowc():
+    return render_template('lowc.html')
+
+@app.route('/saba')
+def saba():
+    return render_template('saba.html')
+    
+@app.route('/dev')
+def dev():
+    return render_template('dev.html')
+    
+@app.route('/fol')
+def fol():
+    return render_template('fol.html')
+
+@app.route('/dar')
+def dar():
+    return render_template('dar.html')
+
+@app.route('/uni')
+def uni():
+    return render_template('uni.html')    
+
+@app.route('/tutorial')
+def tutorial():
+    return render_template('tutorial.html')    
+
+@app.route('/coustume')
+def coustume():
+    return render_template('coustume.html') 
+
 # Flask view function for 'search' endpoint
 @app.route('/search')
 def search():
@@ -77,6 +121,28 @@ def signin():
 def dashboard():
     # Render dashboard template
     return render_template('dashboard.html')
+
+
+@app.route('/degrees', methods=['GET', 'POST'])
+def degrees_page():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM universities")
+    universities = cursor.fetchall()
+    cursor.close()
+    
+    if request.method == 'POST':
+        university_id = int(request.form['university'])
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM universities WHERE id = %s", (university_id,))
+        selected_university = cursor.fetchone()
+
+        cursor.execute("SELECT * FROM degrees WHERE university_id = %s", (university_id,))
+        degrees = cursor.fetchall()
+        cursor.close()
+        
+        return render_template('degrees.html', universities=universities, selected_university=selected_university, degrees=degrees)
+    
+    return render_template('degrees.html', universities=universities)
 
 if __name__ == '__main__':
     app.run(debug=True)
