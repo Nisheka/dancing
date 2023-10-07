944fef6518bb73d7cae280795a5afa1a9a0d5a4f
Nisheka <tharushinisheka@gmail.com>
2023-10-07 10:16:45 +0530

    5th commit

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 #app.py
-from flask import Flask, request, session, redirect, url_for, render_template, flash
+from flask import Flask,jsonify, request, session, redirect, url_for, render_template, flash
+from datetime import datetime
 import psycopg2 #pip install psycopg2 
+import csv
+
+
 
 app = Flask(__name__)
 app.secret_key = 'cairocoders-ednalan'
@@ -17,7 +21,6 @@ except psycopg2.OperationalError as e:
     print("Error:", e)
 
 
-app = Flask(__name__)
 app.static_folder = 'static'  # Set static folder as 'static' directory in your Flask application
 
 # Routes
@@ -105,30 +108,103 @@ def signup():
     
     return redirect(url_for('home'))
 
-@app.route('/signin', methods=['POST'])
+
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
-    # Get form data
-    email = request.form['email']
-    password = request.form['password']
-    
-    # Query database for user with given email and password
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
-    user = cursor.fetchone()
-    cursor.close()
-    
-    if user:
-        # Redirect to dashboard page for logged in user
-        return redirect(url_for('dashboard'))
-    else:
-        # Redirect back to home page with error message
-        return redirect(url_for('home', error='Invalid email or password'))
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+        user = cursor.fetchone()
+        cursor.close()
+
+        if user:
+            session['id'] = user[0]  # Store user ID in session
+            role = user[4]  # Get the user role from the database
+            session['name'] = user[1] 
+            
+
+            if role == 'event_manager':
+
+                return redirect(url_for('event'))  # Redirect to event manager dashboard
+
+            elif role == 'admin':
+                # Redirect to admin dashboard
+                # Add admin dashboard route here
+                pass
+            elif role == 'artist':
+                # Redirect to artist dashboard
+                # Add artist dashboard route here
+                pass
+            else:
+                return redirect(url_for('dashboard'))  # Redirect to user dashboard
+        else:
+            return redirect(url_for('sign', error='Invalid email or password'))
+
+    return render_template('sign.html')
+
+@app.route('/signout')
+def signout():
+    session.pop('id', None)  # Remove user ID from session
+    return redirect(url_for('event'))
+
+
+
 
 @app.route('/dashboard')
 def dashboard():
     # Render dashboard template
     return render_template('dashboard.html')
+    
+@app.route('/event', methods=['GET', 'POST'])
+def event():
+    if 'id' not in session:
+        return redirect(url_for('signin'))
 
+    if request.method == 'POST':
+        # Get event details from the form
+        event_date = request.form['event_date']
+        start_time = request.form['start_time']
+        end_time = request.form['end_time']
+        break_time = request.form['break_time']
+        sessions = request.form['sessions']
+        lead = request.form['lead']
+
+        # Insert event details into the events table
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO events (event_date, start_time, end_time, break_time, sessions, lead) VALUES (%s, %s, %s, %s, %s, %s)",
+                       (event_date, start_time, end_time, break_time, sessions, lead))
+        conn.commit()
+        cursor.close()
+
+        return redirect(url_for('calendar'))
+
+    return render_template('event.html')
+
+
+
+@app.route('/get_events')
+def get_events():
+    # Fetch event data from the database and return it as JSON
+    start = datetime.fromisoformat(request.args.get('start'))
+    end = datetime.fromisoformat(request.args.get('end'))
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM events WHERE event_date BETWEEN %s AND %s", (start.date(), end.date()))
+    events = cursor.fetchall()
+    cursor.close()
+
+    event_list = []
+    for event in events:
+        event_list.append({
+            'lead': event[6],  # Assuming 'lead' is the 7th column in your events table
+            'start': event[2].isoformat(),  # Assuming 'event_date' is the 3rd column
+            'end': event[3].isoformat()     # Assuming 'end_time' is the 4th column
+        })
+
+    return jsonify({'events': event_list})
 
 @app.route('/degrees', methods=['GET', 'POST'])
 def degrees_page():
@@ -151,5 +227,126 @@ def degrees_page():
     
     return render_template('degrees.html', universities=universities)
 
+
+
+@app.route('/account', methods=['GET', 'POST'])
+def account():
+    if 'id' in session:
+        id = session['id']
+        email = session.get('email', None)
+        cursor = conn.cursor()
+        if request.method == 'POST':
+            name = request.form['name']
+            email = request.form['email']
+            # Update user details in the database
+            cursor.execute("UPDATE users SET name = %s, email = %s WHERE id = %s", (name, email, id))
+            conn.commit()
+            flash('User details updated successfully', 'success')
+        
+        # Fetch user details from the database
+        cursor.execute("SELECT * FROM users WHERE id = %s", (id,))
+        user = cursor.fetchone()
+        cursor.close()
+        
+        return render_template('account.html', user=user)
+    else:
+        return redirect(url_for('signin'))
+
+
+
+
+@app.route('/update_user', methods=['POST'])
+def update_user():
+    if 'id' in session:
+        user_id = session['id']
+        name = request.form['name']
+        email = request.form['email']
+        
+        cursor = conn.cursor()
+        cursor.execute("UPDATE users SET name = %s, email = %s WHERE id = %s", (name, email, user_id))
+        conn.commit()
+        cursor.close()
+
+        # Redirect to my_account page after updating
+        return redirect(url_for('account'))
+    
+    # Redirect to login if user not logged in or other error occurs
+    return redirect(url_for('signin'))
+
+    
+    # Redirect to login if user not logged in or other error occurs
+    return redirect(url_for('signin'))
+
+# ----------- Second Flask Application Routes -----------
+# Define a function to recommend dance style based on user input
+def recommend_dance_style(user_input):
+    # Load dance style data from a CSV file (replace 'dance_styles.csv' with your CSV file)
+    dance_styles = []
+    with open('dance_style.csv', 'r', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            dance_styles.append(row)
+
+    # Define a function to calculate a match score between user input and dance styles
+    def calculate_match_score(dance_style, user_input):
+        # Initialize match score to 0
+        match_score = 0
+
+        # Check if required keys are present in dance_style and user_input dictionaries
+        if 'age_min' in dance_style and 'age_max' in dance_style and 'age' in user_input:
+            if int(dance_style.get('age_min', 0)) <= int(user_input['age']) <= int(dance_style.get('age_max', float('inf'))):
+                match_score += 1
+
+        # Check if 'Previous Dance Experience' is present in dance_style and user_input dictionaries
+        if 'Previous Dance Experience' in dance_style and 'experience' in user_input:
+            if dance_style['Previous Dance Experience'].lower() == user_input['experience'].lower():
+                match_score += 1
+
+        # Check if 'Goals' is present in dance_style and user_input dictionaries
+        if 'Goals' in dance_style and 'goals' in user_input:
+            if dance_style['Goals'].lower() == user_input['goals'].lower():
+                match_score += 1
+
+        return match_score
+
+    # Calculate match scores for each dance style
+    match_scores = {}
+    for style in dance_styles:
+        match_scores[style.get('Recommended Dance Style', 'Unknown')] = calculate_match_score(style, user_input)
+
+    # Get the dance style with the highest match score
+    recommended_style = max(match_scores, key=match_scores.get)
+
+    return recommended_style
+
+@app.route('/dance-recommendation', methods=['GET', 'POST'])
+def dance_recommendation():
+    recommended_style = None
+    user_input = {}  # Define an empty dictionary for user input
+
+    if request.method == 'POST':
+        user_input = {
+            'age': request.form['age'],
+            'experience': request.form['experience'],
+            'inspiration': request.form['inspiration'],
+            'goals': request.form['goals'],
+            'styles': request.form['styles'],
+            'explore': request.form['explore'],
+            'limitations': request.form['limitations'],
+            'interest': request.form['interest'],
+            'artist': request.form['artist'],
+            'flexibility_coordination': request.form['flexibility'],
+            'solo_partner_group': request.form['solo_partner_group'],
+            'music_genre': request.form['music_genre']
+            # Add more input fields as needed
+        }
+
+        # Call the recommend_dance_style function
+        recommended_style = recommend_dance_style(user_input)
+
+    return render_template('index.html', recommended_style=recommended_style)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
